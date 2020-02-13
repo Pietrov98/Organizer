c446cd14bdd8a2a58fd04d3147aae151eac42b39
Pietrov98 <adrian.pietruszka98@gmail.com>
2020-02-13 11:56:28 +0100

    First steps in Android Studio

---
+++ app/src/main/java/com/example/organizer/AddMemento.java
@@ -0,0 +1,18 @@
+package com.example.organizer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.view.View;
+import android.widget.TextView;
+
+public class AddMemento extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_memento);
+    }
+}
---
+++ app/src/main/java/com/example/organizer/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.organizer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        createActivitiesButtons();
+
+        Button makeMementoButton = findViewById(R.id.makeMementoButton);
+        makeMementoButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view)
+            {
+                createMemento();
+            }
+        });
+    }
+
+    private void createMemento()
+    {
+        Intent intent = new Intent(this, AddMemento.class);
+        startActivity(intent);
+    }
+
+    private void createActivitiesButtons() {
+
+        for (int i = 0; i < 3; i++)
+        {
+            Button myButton = new Button(this);
+            myButton.setText("Button :" + i);
+            myButton.setId(i);
+            final int id_ = myButton.getId();
+
+            LinearLayout layout = (LinearLayout) findViewById(R.id.activityButtons);
+            layout.addView(myButton);
+
+        }
+    }
+}
